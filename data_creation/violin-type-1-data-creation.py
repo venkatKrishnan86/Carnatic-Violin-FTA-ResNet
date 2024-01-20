957d3cdb681216897498eb357b30ff455f6de972
venkatKrishnan86 <88517272+venkatKrishnan86@users.noreply.github.com>
2024-01-20 09:27:42 -0500

    Updated by removing default loc

---
+++ data_creation/violin-type-1-data-creation.py
@@ -0,0 +1,23 @@
+import os
+import torch
+import torchaudio
+import argparse
+
+parser = argparse.ArgumentParser(
+                    prog='Violin Type-1 Data Creation',
+                    description='Creates a .pt file storing all violin audios as torch Tensors in a list')
+
+parser.add_argument('--location')
+args = parser.parse_args()
+loc = args.location
+
+wavFilePaths = []
+ext = ".wav"
+for (path, dirs, files) in os.walk(loc):
+    for f in files:
+        if ext.split('.')[-1].lower() == f.split('.')[-1].lower():
+            wavFilePaths.append(path + "/" + f)
+
+violin = [torchaudio.load(i)[0][0] for i in wavFilePaths]
+torch.save(violin, './data/carnatic_violin_dataset.pt')
+print("Completed!")
