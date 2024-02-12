f809d7137c6092740a910f50a3477b5e6e64c083
venkatKrishnan86 <88517272+venkatKrishnan86@users.noreply.github.com>
2024-02-12 10:58:18 -0500

    Removed writePath

--- pitchExtractionMelodia.py
+++ pitchExtractionMelodia.py
@@ -12,7 +12,6 @@ parser = argparse.ArgumentParser("Melodia Pitch Extractor",
                                  description="Read Audio, and write melodia output")
 
 parser.add_argument("audioPath")
-parser.add_argument("writePath")
 parser.add_argument("--sampleRate", default=44100)
 parser.add_argument("--frameSize", default=2048)
 parser.add_argument("--hopSize", default=128)
@@ -21,7 +20,6 @@ parser.add_argument("--format", default='wav')
 args = parser.parse_args()
 
 audiofile = args.audioPath
-writePath = args.writePath
 sr = args.sampleRate
 frameSize = args.frameSize
 hopSize = args.hopSize
@@ -49,15 +47,15 @@ pitch_values, pitch_confidence = pitch_extractor(audio)
 
 pitch_times = np.linspace(0.0, len(audio)/sr, len(pitch_values))
 
+np.savetxt("./result/MELODIA_"+audioFolder+"/pitch.txt", np.stack([pitch_times, pitch_values], axis = -1))
+
 # Plot the estimated pitch contour and confidence over time.
 f, axarr = plt.subplots(2, sharex=True)
 axarr[0].plot(pitch_times, pitch_values)
 axarr[0].set_title('estimated pitch [Hz]')
 axarr[1].plot(pitch_times, pitch_confidence)
 axarr[1].set_title('pitch confidence')
-f.savefig("result/MELODIA_"+audioFolder+"/plot.pdf")
-
-np.savetxt("result/MELODIA_"+audioFolder+"/pitch.txt", np.stack([pitch_times, pitch_values], axis = -1))
+f.savefig("./result/MELODIA_"+audioFolder+"/plot.pdf")
 
 # Essentia operates with float32 ndarrays instead of float64, so let's cast it.
 synthesized_melody = pitch_contour(pitch_times, pitch_values, sr).astype(np.float32)[:len(audio)]
